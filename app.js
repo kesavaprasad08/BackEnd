24e63f430e287458f3cbb3fa952a72710574e11b
kesavaprasad08 <kesav2661998@gmail.com>
2024-05-04 12:44:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const functions =require("firebase-functions")
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const userRoutes = require("./routes/user");
+const scriptRoutes = require("./routes/script");
+app.use("/script", scriptRoutes);
+app.use("/auth", userRoutes);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((res) => {
+    app.listen(4000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  exports.api = functions.https.onRequest(app)
